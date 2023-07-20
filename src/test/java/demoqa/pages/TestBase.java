cbd00b2a4eae592bd202d9458056db1bf982acf0
nikonov1996 <ibigmeat1996@gmail.com>
2023-07-20 19:54:29 +0300

    added allure report and allure steps for /text-box page test

--- src/test/java/demoqa/pages/TestBase.java
+++ src/test/java/demoqa/pages/TestBase.java
@@ -1,7 +1,9 @@
 package demoqa.pages;
 
 import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.logevents.SelenideLogger;
 import io.github.bonigarcia.wdm.WebDriverManager;
+import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.BeforeAll;
 
 import static com.codeborne.selenide.Selenide.executeJavaScript;
@@ -16,6 +18,7 @@ public class TestBase {
 
     @BeforeAll
     static void before(){
+        SelenideLogger.addListener("allure", new AllureSelenide());
         WebDriverManager.chromedriver().setup();
         Configuration.browser = "chrome";
         Configuration.driverManagerEnabled = true;
