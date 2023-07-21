bef6f297cc5bd2899f1dbb642ad1a1f52ddfcf10
nikonov1996 <ibigmeat1996@gmail.com>
2023-07-21 12:29:02 +0300

    add github action example with test running

--- src/test/java/demoqa/TextBoxTest.java
+++ src/test/java/demoqa/TextBoxTest.java
@@ -6,7 +6,9 @@ import com.codeborne.selenide.SelenideElement;
 import demoqa.model.RegisterUser;
 import demoqa.pages.TestBase;
 import io.github.bonigarcia.wdm.WebDriverManager;
+import io.qameta.allure.*;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.chrome.ChromeOptions;
@@ -18,19 +20,47 @@ import static java.lang.String.format;
 public class TextBoxTest extends TestBase {
 
     @Test
-    void testThatTextBoxSubmitSuccess(){
+    @Owner("Никонов В.А. @qanva")
+    @Severity(SeverityLevel.BLOCKER)
+    @Links(value = {
+            @Link(name = "/text-box form", url = "https://demoqa.com/text-box"),
+            @Link(name = "base page", url = "https://demoqa.com")
+    })
+    @Story("Заполнение формы /text-box")
+    @Feature("Форма /text-box")
+    @DisplayName("Заполнение формы /text-box корректными данными")
+    void testThatTextBoxSubmitSuccess() {
         RegisterUser expectedUser = generateRegisterUser();
 
         textBoxPage
                 .openPage()
                 .setFullName(
                         format("%s %s",
-                        expectedUser.getFirstName(),
-                        expectedUser.getLastName()))
+                                expectedUser.getFirstName(),
+                                expectedUser.getLastName()))
                 .setEmail(expectedUser.getEmail())
                 .setAddress(expectedUser.getAddress())
                 .setPermanentAddress(expectedUser.getAddress())
                 .submitForm()
                 .verifyThatOutputFormPresent();
+    }
+
+    @Test
+    /*
+       Данный тест, показывает возможность работы с Allure
+       без использования аннотаций.
+       Динамические тэги, нужны для расширения возможностей: например,
+       добавить общий story или feature для группы тестов.
+    */
+    void testDinamicAllureAnnotation() {
+        Allure.getLifecycle().updateTestCase(
+                testResult ->
+                        testResult.setName("Тест, демонстрирующий работу Allure без аннотаций"));
+        Allure.feature("Allure описание тест кейса");
+        Allure.story("Описание тест кейса в Allure без использования аннотаций");
+        Allure.label("owner", "Никонов В.А nikonov1996");
+        Allure.label("severity", SeverityLevel.NORMAL.value());
+        Allure.link("Форма /text-box", "https://demoqa.com/text-box");
+        Allure.description("Данный тест не содержит шагов, только  Allure описание ");
     }
 }
