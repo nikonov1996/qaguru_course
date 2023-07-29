295646cfd18ed73d9b3ebf70a1ac046e157c3af1
nikonov1996 <ibigmeat1996@gmail.com>
2023-07-29 14:35:49 +0300

    added telegram allure notifications

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,35 +1,7 @@
 package org.example;
 
-import static org.example.Days.MONDAY;
-import static org.example.Days.SUNDAY;
-
 public class Main {
     public static void main(String[] args) {
 
-        String i = "воскресенье";
-        System.out.println(Days.valueOf(SUNDAY.name()));
-
-    }
-}
-
-enum Days{
-    SUNDAY("воскресенье"),
-    MONDAY("понедельник");
-
-    private String title;
-
-    Days(String title) {
-        this.title = title;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    @Override
-    public String toString() {
-        return "DayOfWeek{" +
-                "title='" + title + '\'' +
-                '}';
     }
 }
